e644aa473b52fa7d8d94da2d0449e1327275df3e
douglasvervalen <69044467+douglasvervalen@users.noreply.github.com>
2021-10-02 15:39:30 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -15,15 +15,15 @@ double valorA,valorB,valorcpf;
        valorB=valorcpf/100; // Separa os primeiros dígitos como a parte inteira. No valor de exemplo o CPF fica como: 123,55.
 
        valorA=(valorB-floor(valorB))* 100; /* Separa os dois últimos dígitos como a parte decimal.
-       No valor de exemplo o CPF fica depois da primeira operação como: 0,55, e então multiplicando por 100 fica: 55 */
+       No valor de exemplo o CPF depois da primeira operação fica como: 0,55, e então multiplicando por 100 fica: 55 */
 
        int bint=floor(valorB); /* Converte o valor de B para um número inteiro.
-       No valor de exemplo como o CPF tem um valor decimal igual ou maior que 5 precisamos que ele arredonde
-       o valor para baixo e não para cima como deveria ser, então usamos a função
-       floor() que permite arredondar para baixo um número decimal. */
+       No valor de exemplo como o CPF tem um valor decimal igual ou maior que 5 (123,55), então precisamos que ele arredonde
+       o valor para baixo e não para cima como deveria ser em um arredondamento padrão, então usamos a função
+       floor() que permite arredondar para baixo um valor decimal. */
 
        int aint= round(valorA); /* Converte o valor de A para um número inteiro. No nosso exemplo o valor de A já é um "inteiro" (55),
-       mas o computador não o reconhece assim, para ele o valor de A é: 55,0, é por isso a necessidade da conversão.  */
+       mas o computador não o reconhece assim, para ele o valor de A é igual: 55,0. Por isso então a necessidade da conversão.  */
 
        if(valorA==0) // Verifica se o valor de A é = 0.
        {
