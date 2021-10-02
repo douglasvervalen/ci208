5d27d115c55193f64eaaf2c3b3107dd4d4f5326f
douglasvervalen <69044467+douglasvervalen@users.noreply.github.com>
2021-10-02 17:40:15 -0300

    CPF Fajuto

--- main.cpp
+++ main.cpp
@@ -17,12 +17,12 @@ double n1 = 1;
        valorA = (valorB - floor(valorB)) * 100; /* Separa os dois últimos dígitos e passa para a parte inteira.
        No valor de exemplo o CPF depois da primeira operação fica como: 0,55, e então multiplicando por 100 fica: 55 */
 
-       int bint=floor(valorB); /* Converte o valor de B para um número inteiro.
+       int bint = floor(valorB); /* Converte o valor de B para um número inteiro.
        No valor de exemplo como o CPF tem um valor decimal igual ou maior que 5 (123,55), então precisamos que ele arredonde
        o valor para baixo e não para cima como deveria ser em um arredondamento padrão, então usamos a função
        floor() que permite arredondar para baixo um valor decimal. */
 
-       int aint= round(valorA); /* Converte o valor de A para um número inteiro. No nosso exemplo o valor de A já é um "inteiro" (55),
+       int aint = round(valorA); /* Converte o valor de A para um número inteiro. No nosso exemplo o valor de A já é um "inteiro" (55),
        mas o computador não o reconhece assim, para ele o valor de A é igual: 55,0. Por isso então a necessidade da conversão.  */
 
        if(valorA==0) // Verifica se o valor de A é = 0.
